5cd4961b8ee72abf36d4f4462679fc2d04f45bfc
lizeshakya <lizeshakya@gmail.com>
2022-10-12 11:25:38 +0545

    Added todo checklist course

---
+++ commands.md
@@ -0,0 +1,7 @@
+* ng serve
+In order to run the project, use the serve command
+
+* ng add @angular/material@13.0.2 --defaults
+
+  To add the material package to the project
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {TodoItem} from "./todoItem";
+import {TodoList} from "./todoList";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css'],
 })
+
 export class AppComponent {
-  title = 'todo';
+  private list = new TodoList("Bob", [
+    new TodoItem("Go for the run", true),
+    new TodoItem("Get flower"),
+    new TodoItem("Collect tickets"),
+  ])
+
+  get username(): string {
+    return this.list.user;
+  }
+
+  get itemCount(): number {
+    return this.list.items.filter(item => !item.complete).length;
+  }
+
+  get items(): readonly TodoItem[] {
+    return this.list.items.filter(item => this.showComplete || !item.complete);
+  }
+
+  addItem(newItem: string) {
+    if (newItem != "") {
+      this.list.addItem(newItem);
+    }
+  }
+
+  showComplete: boolean = false;
 }
